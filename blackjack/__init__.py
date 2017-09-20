93dfef3607a2866dd28037eb21cb2bbea1791208
janvincke <jan.vincke@mobilityhouse.com>
2017-09-20 11:10:55 +0200

    Check the suit of card
    I added a check if the chosen suit of the card is valid or not.

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,5 +1,8 @@
 class Card:
     def __init__(self, suit):
+        if suit not in ['diamond', 'heart', 'spade', 'club']:
+            raise ValueError('This is a standard card game. Suit must be in '
+                             'diamond, heart, spade, club')
         self.suit = suit
 
 
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -1,3 +1,5 @@
+import pytest
+
 from blackjack import Card, Deck
 
 
@@ -10,5 +12,13 @@ def test_deck():
 
 
 def test_card_has_suit():
-    card = Card('diamonds')
+    card = Card('diamond')
     assert hasattr(card, 'suit')
+
+
+def test_card_has_valid_suit():
+    with pytest.raises(ValueError):
+        Card('robotrics')
+
+    c = Card('spade')
+    assert c.suit == 'spade'
