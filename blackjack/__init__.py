c1b071dda2f9361f73bf3259c48f33673c684087
janvincke <jan.vincke@mobilityhouse.com>
2017-09-21 10:21:26 +0200

    Add value to class Card
    The value has to be a number between 2 and 11 or one of the Values A, K, Q or J

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,9 +1,15 @@
 class Card:
-    def __init__(self, suit):
+
+    def __init__(self, suit, value):
         if suit not in ['diamond', 'heart', 'spade', 'club']:
             raise ValueError('This is a standard card game. Suit must be in '
                              'diamond, heart, spade, club')
         self.suit = suit
+
+        if value not in ['A', 'K', 'Q', 'J'] and value not in list(range(2,11)):
+            raise ValueError
+
+        self.value = value
 
 
 class Deck:
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -12,13 +12,22 @@ def test_deck():
 
 
 def test_card_has_suit():
-    card = Card('diamond')
+    card = Card('diamond', 2)
     assert hasattr(card, 'suit')
 
 
 def test_card_has_valid_suit():
     with pytest.raises(ValueError):
-        Card('robotrics')
+        Card('robotrics', 2)
 
-    c = Card('spade')
+    c = Card('spade', 2)
     assert c.suit == 'spade'
+
+
+def test_card_has_a_value():
+
+    king_of_spades = Card('spade', 'K')
+    queen_of_spades = Card('spade', 'Q')
+
+    assert king_of_spades.value == 'K'
+    assert queen_of_spades.value == 'Q'
